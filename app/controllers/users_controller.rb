23c60b1cd9c8259a1b2205eeff639e3cec9a201b
NRM129 <i.naru129@yahoo.ne.jp>
2023-03-07 14:36:36 +0900

    管理ページの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
   
+  def index
+    @notes = Note.page(params[:page]).per(31).order(record_date: :DESC)
+  end
   
   def show
     # binding.pry
